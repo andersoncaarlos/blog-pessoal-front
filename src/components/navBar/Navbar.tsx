f8128aeeb6d31562192341e3fa73e65bfa32a600
Anderson <ander_son12@hotmail.com>
2024-02-19 14:23:28 -0300

    Atualizações de Cadastro e Login

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+
 
 function Navbar() {
  
@@ -6,7 +7,7 @@ function Navbar() {
 
   return (
     <>
-     <div className='w-full bg-auto bg-indigo-900 text-white flex justify-center py-4'>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
           <div className="container flex justify-between text-lg">
             <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
 
