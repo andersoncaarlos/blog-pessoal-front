66e3957474e5e2fbcc0e32cee96202dc12a19c37
Anderson <ander_son12@hotmail.com>
2024-02-21 12:27:12 -0300

    CRUD de Temas

--- src/paginas/cadastro/Cadastro.tsx
+++ src/paginas/cadastro/Cadastro.tsx
@@ -3,10 +3,12 @@ import { useNavigate } from 'react-router-dom'
 import Usuario from '../../models/Usuario'
 import { cadastrarUsuario } from '../../services/Service'
 import './Cadastro.css'
+import { RotatingLines } from 'react-loader-spinner'
 
 function Cadastro() {
 
   let navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false)
 
   const [confirmaSenha, setConfirmaSenha] = useState<string>("")
 
@@ -49,15 +51,18 @@ function Cadastro() {
 
   async function cadastrarNovoUsuario(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault()
+    setIsLoading(true)
 
     if (confirmaSenha === usuario.senha && usuario.senha.length >= 8) {
 
       try {
         await cadastrarUsuario(`/usuarios/cadastrar`, usuario, setUsuarioResposta)
         alert('Usuário cadastrado com sucesso')
+        setIsLoading(false)
 
       } catch (error) {
         alert('Erro ao cadastrar o Usuário')
+        setIsLoading(false)
       }
 
     } else {
@@ -137,9 +142,16 @@ function Cadastro() {
             <button className='rounded text-white bg-red-400 hover:bg-red-700 w-1/2 py-2' onClick={back}>
               Cancelar
             </button>
-            <button className='rounded text-white bg-indigo-400 hover:bg-indigo-900 w-1/2 py-2' type='submit'>
-              Cadastrar
-            </button>
+            <button  type='submit' className="rounded bg-indigo-400 hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
+           {isLoading ? <RotatingLines
+            strokeColor="white"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="24"
+            visible={true}
+          /> :
+            <span>Cadastrar</span>}
+          </button>
           </div>
         </form>
       </div>
