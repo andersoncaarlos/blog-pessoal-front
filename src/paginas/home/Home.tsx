4271bc806941bed32e4d6f5eee46a5182c8f75bc
Anderson <ander_son12@hotmail.com>
2024-02-26 12:34:13 -0300

    CRUD postagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,4 +1,7 @@
 import './Home.css';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+
 
 
 function Home() {
@@ -11,13 +14,13 @@ function Home() {
               <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
