52729c237e61ff72b763706bc1422392ac5fde8a
Anderson <ander_son12@hotmail.com>
2024-02-27 11:23:03 -0300

    Perfil

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,3 +1,4 @@
+import homeLogo from '../../assets/home.png'
 import './Home.css';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
@@ -17,6 +18,11 @@ function Home() {
               <ModalPostagem />
               <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
             </div>
+            </div>
+  
+            <div className="flex justify-center ">
+              <img src={homeLogo} alt="" className='w-2/3' />
+      
             </div>
           </div>
         </div>
